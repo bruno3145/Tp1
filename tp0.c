6489a99fe496f4c35136e73c4c7114d012677805
bruno3145 <31559311+bruno3145@users.noreply.github.com>
2017-09-06 03:02:05 -0300

    Update tp0.c
    O código foi modularizado. 
    Problema: Transição brusca do movimento quando o objeto passa pelo eixo da origem.

--- tp0.c
+++ tp0.c
@@ -1,11 +1,12 @@
 #include <GL/glew.h>
 #include<stdio.h>
 #include <GL/freeglut.h>
+#include "funcoes.h"
 int x=0, y=0, ww=0, dd=0, aa=0, ss=0;
 
 void square(int k, int l){
 
-	glBegin(GL_TRIANGLE_STRIP);
+	glBegin(GL_POLYGON);
         glVertex3f(k, l, 0);
         glVertex3f(k+10, l, 0);
         glVertex3f(k+10, l+10, 0);
@@ -14,32 +15,11 @@ void square(int k, int l){
 }
 
 void reBuffer(){
-	if(x>100){
-		x=0;
-	}
-	if(y>100){
-		y=0;
-	}
-	if(y<0){
-		y+=100;
-	}
-	if(x<0){
-		x+=100;
-	}
-	 if(ww==1){
-		y++;
-	 }
-	 if(ss==1){
-		y--;
-	 }
-	 if(aa==1){
-		x--;
-	 }
-	if(dd==1){
-		x++;
-	}
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(0, 0, 1);
+	int* xx=&x;
+	int* yy=&y;
+	atravessaParede(xx,yy,&aa,&ww,&ss,&dd);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0, 0, 1);
 	square(x,y);
 	glutSwapBuffers();
 }
@@ -47,106 +27,81 @@ void reBuffer(){
 // Inicia algumas variáveis de estado
 void inicializa(void)
 {
-    // cor para limpar a tela
-    glClearColor(1, 1, 1, 0);      // branco
+	// cor para limpar a tela
+	glClearColor(1, 1, 1, 0);      // branco
 }
 
 // Callback de redimensionamento
-void redimensiona(int w, int h)
-{
-   glViewport(0, 0, w, h);
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   glOrtho(0, 100, 0, 100, -1, 1);
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
+void redimensiona(int w, int h){
+
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, 100, 0, 100, -1, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
 // Callback de evento de teclado
-void teclado(unsigned char key)
-{
-   switch(key)
-   {
-      // Tecla ESC
-      case 27:
-         exit(0);
-         break;
-      case 'w':
-	aa=0;
-	ss=0;
-	dd=0;
-	if(ww==1){
-		ww=0;
+void teclado(unsigned char key){
+
+	switch(key){
+		int* a =&aa;
+		int* s =&ss;
+		int* d =&dd;
+		int* w =&ww;
+		// Tecla ESC
+		case 27:
+			 exit(0);
+			 break;
+		case 'w':
+			movimentoTeclas(key,a,w,s,d);
+			glutPostRedisplay();
+			break;
+		case 's':
+			movimentoTeclas(key,a,w,s,d);
+			glutPostRedisplay();
+			break;
+		case 'd':
+			movimentoTeclas(key,a,w,s,d);
+			glutPostRedisplay();
+			break;
+		case 'a':
+			movimentoTeclas(key,a,w,s,d);
+			glutPostRedisplay();
+			break;
+		default:
+			 break;
 	}
-	else
-	ww=1;
-	glutPostRedisplay();
-	break;
-      case 's':
-	aa=0;
-	ww=0;
-	dd=0;
-	if(ss==1){
-		ss=0;
-	}
-	else
-	ss=1;
-	glutPostRedisplay();
-	break;
-      case 'd':
-	aa=0;
-	ss=0;
-	ww=0;
-	if(dd==1){
-		dd=0;
-	}
-	else
-	dd=1;
-	glutPostRedisplay();
-	break;
-      case 'a':
-	ww=0;
-	ss=0;
-	dd=0;
-	if(aa==1){
-		aa=0;
-	}
-	else
-	aa=1;
-	glutPostRedisplay();
-	break;
-      default:
-         break;
-   }
 }
 
 
 // Rotina principal
-int main(int argc, char **argv)
-{
-    // Acordando o GLUT
-    glutInit(&argc, argv);
+int main(int argc, char **argv){
 
-    // Definindo a versão do OpenGL que vamos usar
-    glutInitContextVersion(1, 1);
-    glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+	// Acordando o GLUT
+	glutInit(&argc, argv);
 
-    // Configuração inicial da janela do GLUT
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(400, 400);
-    glutInitWindowPosition(100, 100);
+	// Definindo a versão do OpenGL que vamos usar
+	glutInitContextVersion(1, 1);
+	glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
 
-    // Abre a janela
-    glutCreateWindow("tp1");
+	// Configuração inicial da janela do GLUT
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(400, 400);
+	glutInitWindowPosition(100, 100);
 
-    // Registra callbacks para alguns eventos
-    glutDisplayFunc(reBuffer);
-    glutReshapeFunc(redimensiona);
-    glutKeyboardFunc(teclado);
-    glutIdleFunc(glutPostRedisplay);
-    inicializa();
+	// Abre a janela
+	glutCreateWindow("Tp1.io");
 
-    // Entra em loop e nunca sai
-    glutMainLoop();
-    return 0;
+	// Registra callbacks para alguns eventos
+	glutDisplayFunc(reBuffer);
+	glutReshapeFunc(redimensiona);
+	glutKeyboardFunc(teclado);
+	glutIdleFunc(glutPostRedisplay);
+	inicializa();
+
+	// Entra em loop e nunca sai
+	glutMainLoop();
+	return 0;
 }
