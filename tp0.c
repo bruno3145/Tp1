fe4eace3b6fb784f256b4f5e768bbd61a8051da7
bruno3145 <31559311+bruno3145@users.noreply.github.com>
2017-09-07 06:09:05 -0300

    Add files via upload
    Bug resolvido, agora o código funciona.

--- tp0.c
+++ tp0.c
@@ -1,10 +1,46 @@
+//#include "funcoes.h"
 #include <GL/glew.h>
 #include<stdio.h>
 #include <GL/freeglut.h>
-#include "funcoes.h"
-int x=0, y=0, ww=0, dd=0, aa=0, ss=0;
 
-void square(int k, int l){
+#define LARGURA_DO_MUNDO 160
+#define ALTURA_DO_MUNDO 90
+
+int axisx=0, axisy=0,up=0,down=0,left=0,right=0;
+void movimenta(){
+
+	if(up==1){
+		glColor3f(0, 0, 1);
+	 axisy++;
+	}
+	if(down==1){
+		glColor3f(0, 1, 1);
+	 axisy--;
+	}
+	if(left==1){
+		glColor3f(1, 0, 1);
+	 axisx--;
+	}
+ if(right==1){
+				glColor3f(1, 0, 0);
+	 axisx++;
+ }
+}
+
+void resetaMovimento(){
+	up=0;
+	down=0;
+	left=0;
+	right=0;
+}
+/*void worldShape(){
+
+	glColor3f(0, .5, 0);  // verde escuro
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	desenhaRetangulo(1, 1, LARGURA_DO_MUNDO-2, ALTURA_DO_MUNDO-2);
+}*/
+
+void characterShape(int k, int l){
 
 	glBegin(GL_POLYGON);
         glVertex3f(k, l, 0);
@@ -15,24 +51,19 @@ void square(int k, int l){
 }
 
 void reBuffer(){
-	int* xx=&x;
-	int* yy=&y;
-	atravessaParede(xx,yy,&aa,&ww,&ss,&dd);
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(0, 0, 1);
-	square(x,y);
+	movimenta();
+	characterShape(axisx,axisy);
 	glutSwapBuffers();
 }
 
-// Inicia algumas variáveis de estado
-void inicializa(void)
-{
+void inicializa(void){
 	// cor para limpar a tela
 	glClearColor(1, 1, 1, 0);      // branco
 }
 
 // Callback de redimensionamento
-void redimensiona(int w, int h){
+/*void redimensiona(int w, int h){
 
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
@@ -40,38 +71,66 @@ void redimensiona(int w, int h){
 	glOrtho(0, 100, 0, 100, -1, 1);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
+}*/
+
+void redimensiona(int width, int height) {
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, LARGURA_DO_MUNDO, 0, ALTURA_DO_MUNDO, -1, 1);
+
+    float razaoAspectoJanela = ((float)width)/height;
+    float razaoAspectoMundo = ((float) LARGURA_DO_MUNDO)/ ALTURA_DO_MUNDO;
+    // se a janela está menos larga do que o mundo (16:9)...
+    if (razaoAspectoJanela < razaoAspectoMundo) {
+        // vamos colocar barras verticais (acima e abaixo)
+        float hViewport = width / razaoAspectoMundo;
+        float yViewport = (height - hViewport)/2;
+        glViewport(0, yViewport, width, hViewport);
+    }
+    // se a janela está mais larga (achatada) do que o mundo (16:9)...
+    else if (razaoAspectoJanela > razaoAspectoMundo) {
+        // vamos colocar barras horizontais (esquerda e direita)
+        float wViewport = ((float)height) * razaoAspectoMundo;
+        float xViewport = (width - wViewport)/2;
+        glViewport(xViewport, 0, wViewport, height);
+    } else {
+        glViewport(0, 0, width, height);
+    }
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
 
 // Callback de evento de teclado
-void teclado(unsigned char key){
+void teclado(unsigned char key, int A, int B){
 
 	switch(key){
-		int* a =&aa;
-		int* s =&ss;
-		int* d =&dd;
-		int* w =&ww;
-		// Tecla ESC
-		case 27:
-			 exit(0);
-			 break;
-		case 'w':
-			movimentoTeclas(key,a,w,s,d);
-			glutPostRedisplay();
-			break;
-		case 's':
-			movimentoTeclas(key,a,w,s,d);
-			glutPostRedisplay();
-			break;
-		case 'd':
-			movimentoTeclas(key,a,w,s,d);
-			glutPostRedisplay();
-			break;
-		case 'a':
-			movimentoTeclas(key,a,w,s,d);
-			glutPostRedisplay();
-			break;
-		default:
-			 break;
+
+			case 27:
+				exit(0);
+				break;
+			case 'w':
+				resetaMovimento();
+				up=1;
+				glutPostRedisplay();
+				break;
+			case 's':
+				resetaMovimento();
+				down=1;
+				glutPostRedisplay();
+				break;
+			case 'd':
+				resetaMovimento();
+				right=1;
+				glutPostRedisplay();
+				break;
+			case 'a':
+				resetaMovimento();
+				left=1;
+				glutPostRedisplay();
+				break;
+			default:
+				break;
 	}
 }
 
@@ -92,7 +151,7 @@ int main(int argc, char **argv){
 	glutInitWindowPosition(100, 100);
 
 	// Abre a janela
-	glutCreateWindow("Tp1.io");
+	glutCreateWindow("Tp1");
 
 	// Registra callbacks para alguns eventos
 	glutDisplayFunc(reBuffer);
