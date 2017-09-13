c219fa5f28f293c4f20e2ee23c6aaf9ded163f87
bruno3145 <31559311+bruno3145@users.noreply.github.com>
2017-09-12 23:50:07 -0300

    Add files via upload
    +Sistema de tiro;
    +Tela de pause;
    +Inimigos vindo em waves;

--- main.c
+++ main.c
@@ -3,72 +3,194 @@
 #include <GL/freeglut.h>
 #include <stdio.h>
 #include <math.h>
+#include <SOIL/SOIL.h>
 
+#define radianoParaGraus(radianos) (radianos * (180.0 / M_PI))
+#define grausParaRadianos(graus) ((graus * M_PI) / 180.0)
+#define LARGURA_DO_MUNDO 800
+#define ALTURA_DO_MUNDO 600
+
+const float velocidadeTiro=15.00f;
+const int MAX_ENEMIES = 1000;
+GLuint texturaBackground;
+GLuint texturaEnemy;
+GLuint texturaPause;
 GLfloat orientacaoEmGraus = 0;
 GLfloat velocidadeAngular = 0.05;
 GLfloat x = 0, y = 0;
 float velocidadeTangencial = 0.5;
 int trueMouseX, trueMouseY;
-#define LARGURA_DO_MUNDO 800
-#define ALTURA_DO_MUNDO 600
 int colidiu =0;
-int enemieColidiu=0;
 int globalWidth=LARGURA_DO_MUNDO, globalHeight= ALTURA_DO_MUNDO;
-#define radianoParaGraus(radianos) (radianos * (180.0 / M_PI))
-#define grausParaRadianos(graus) ((graus * M_PI) / 180.0)
 const int raioEnemy =10;
 const int raioPlayer=10;
 
 
 float axisx=0, axisy=0;
-int up=0,down=0,left=0,right=0,r,enemieX,enemieY,enemieCount=0,level=12;
-int clicou =0,parametroJ;
+int up=0,down=0,left=0,right=0,r,enemyCount=0,level=10,pause = 0;
+int atirou=0, tiroNaTela=0;;
 
 struct ponto {
-   int x, y;
+   float x, y;
 };
-
-struct Enemie{
-	int x;
-	int y;
-};
-
-struct Enemie enemieVector[100];
+struct ponto enemyVector[1000];
 struct ponto posicaoMouse;
 struct ponto cliqueMouse;
 struct ponto tiro;
+struct ponto enemy;
 
-void clickMouse(int button, int estado, int m, int n){
 
-  if (button==GLUT_LEFT_BUTTON){
-    cliqueMouse.x=m;
-    cliqueMouse.y=globalHeight-n;
-    clicou=1;
+void loadEnemy() {
+  texturaEnemy = SOIL_load_OGL_texture("frontwalker2.png",SOIL_LOAD_AUTO,SOIL_CREATE_NEW_ID,SOIL_FLAG_INVERT_Y);
+
+  if (texturaEnemy == 0) {
+    printf("Erro carregando textura: '%s'\n", SOIL_last_result());
   }
+}
+void loadBackground() {
+  texturaBackground = SOIL_load_OGL_texture("backgroundzumbis.png",SOIL_LOAD_AUTO,SOIL_CREATE_NEW_ID,SOIL_FLAG_INVERT_Y);
+
+  if (texturaBackground == 0) {
+    printf("Erro carregando textura: '%s'\n", SOIL_last_result());
+  }
+}
+void loadPause() {
+  texturaPause = SOIL_load_OGL_texture("texturaPause.png",SOIL_LOAD_AUTO,SOIL_CREATE_NEW_ID,SOIL_FLAG_INVERT_Y);
+
+  if (texturaPause == 0) {
+    printf("Erro carregando textura: '%s'\n", SOIL_last_result());
+  }
+}
+void backGround(){
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, texturaBackground);
+	glBegin(GL_TRIANGLE_FAN);
+		glTexCoord2f(0, 0); glVertex3f(0, 0,0);
+		glTexCoord2f(1, 0); glVertex3f( globalWidth, 0,0);
+		glTexCoord2f(1, 1); glVertex3f( globalWidth,  globalHeight,0);
+		glTexCoord2f(0, 1); glVertex3f(0,  globalHeight,0);
+	glEnd();
+	glDisable(GL_TEXTURE_2D);
+}
+
+void drawPauseScreen(){
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, texturaPause);
+	glBegin(GL_TRIANGLE_FAN);
+		glTexCoord2f(0, 0); glVertex3f(0, 0,0);
+		glTexCoord2f(1, 0); glVertex3f( globalWidth, 0,0);
+		glTexCoord2f(1, 1); glVertex3f( globalWidth,  globalHeight,0);
+		glTexCoord2f(0, 1); glVertex3f(0,  globalHeight,0);
+	glEnd();
+  glDisable(GL_TEXTURE_2D);
+}
+
+void createShot(){
+  tiro.x = axisx;
+  tiro.y = axisy;
 }
 
 void drawShot(){
-  glColor3f(1,0,0);
-      glVertex2f(0,-3);
-      glVertex2f(-3,0);
-      glVertex2f(0,3);
-      glVertex2f(3,0);
+  glBegin(GL_POLYGON);
+      glVertex2f(0,-4);
+      glVertex2f(-4,0);
+      glVertex2f(0,4);
+      glVertex2f(4,0);
   glEnd();
+}
+
+void clickMouse(int button, int state, int m, int n){
+
+  if ((button==GLUT_LEFT_BUTTON) && (state==GLUT_DOWN)){
+    cliqueMouse.x=m;
+    cliqueMouse.y=globalHeight-n;
+    if(atirou==0 && tiroNaTela==0){
+      atirou=1;
+    }
+  }
+  //if((button==GLUT_LEFT_BUTTON) && (state==GLUT_UP)){
+    //clicou=0;
+  //}
+}
+
+void tiro_AvaliaPosicao(){  // DETERMINA QUANDO O TIRO SOME A PARTIR DA AVALIAÇÃO DA POSIÇÃO RELATIVA ENTRE O ALVO E O PLAYER
+  if(axisx<cliqueMouse.x){ // player está a esquerda do mouse
+    if(tiro.x<cliqueMouse.x){
+      shotFollows();
+    }
+    else{
+      tiroNaTela=0;
+    }
+  }
+  if(axisx>cliqueMouse.x){ // player está a direita do mouse
+    if(tiro.x>cliqueMouse.x){
+      shotFollows();
+    }
+    else{
+      tiroNaTela=0;
+    }
+  }
+  if(axisy>cliqueMouse.y){ //player está acima do mouse
+    if(tiro.y>cliqueMouse.y){
+      shotFollows();
+    }
+    else{
+      tiroNaTela=0;
+    }
+  }
+  if(axisy<cliqueMouse.y){ // player está abaixo do mouse
+    if(tiro.y<cliqueMouse.y){
+    shotFollows();
+    }
+    else{
+      tiroNaTela=0;
+    }
+  }
+}
+
+void enemySpawn(){
+  if(enemyCount<MAX_ENEMIES && pause==0){
+    switch(enemyCount){
+      case 0 :
+        enemyCount+=10;
+        break;
+      case 10 :
+        enemyCount+=20;
+        break;
+      case 30 :
+        enemyCount+=40;
+        break;
+      case 70 :
+        enemyCount+=80;
+        break;
+      case 150 :
+        enemyCount+=160;
+        break;
+      case 310 :
+        enemyCount+=320;
+        break;
+      case 630:
+        enemyCount+=370;
+    }
+  }
+  glutTimerFunc(30000,enemySpawn,0);
 }
 
 void movimentoMouse(int x, int y) {     //callback do mouse       ELIMINAR FUNCAO CORRIGE MOUSE
     posicaoMouse.x = x;
     posicaoMouse.y = y;
 }
-void createEnemie(){
-	r = globalWidth+(rand()%3300-2600);
-	enemieVector[enemieCount].x = r;
-	r = globalHeight+(rand()%3300-2600);
-	enemieVector[enemieCount].y = r;
+void createEnemy(){
+    for(int i=0;i<1000;i++){
+  	r = globalWidth+(rand()%2300-1600);
+  	enemyVector[i].x = r;
+  	r = globalHeight+(rand()%2300-1600);
+  	enemyVector[i].y = r;
+  }
 }
 
 int colisao (int i) {
-  int distancia = sqrt(((axisx/2 - enemieVector[i].x/2) * (axisx/2 - enemieVector[i].x/2)) + ((axisy/2 - enemieVector[i].y/2) * (axisy/2 - enemieVector[i].y/2)));
+  int distancia = sqrt(((axisx/2 - enemyVector[i].x/2) * (axisx/2 - enemyVector[i].x/2)) + ((axisy/2 - enemyVector[i].y/2) * (axisy/2 - enemyVector[i].y/2)));
   if (axisx>=globalWidth){ //FAZER RETORNAR 1 PARA PODER TIRAR VIDA DO PLAYER
     axisx-=30;
   }
@@ -82,13 +204,13 @@ int colisao (int i) {
     axisy+=30;
   }
   if (distancia <= raioPlayer + raioEnemy){
-        if(axisy>enemieVector[i].y){
+        if(axisy>enemyVector[i].y){
             axisy+=30;
         }
         else
           axisy-=30;
 
-      if(axisx<enemieVector[i].x){
+      if(axisx<enemyVector[i].x){
         axisx-=30;
     }
     else
@@ -99,86 +221,76 @@ int colisao (int i) {
   else
       return 0;
 }
-
-int colisaoEnemie (struct Enemie enemie, struct Enemie enemy) {
-  int distancia = sqrt(((enemie.x/2 - enemy.x/2) * (enemie.y/2 - enemy.x/2)) + ((enemie.y/2 - enemy.y/2) * (enemie.y/2 - enemy.y/2)));
-  if (distancia <= raioPlayer){
-    return 1;
-  }
-  else
-    return 0;
+void drawEnemy(){
+  glEnable(GL_TEXTURE_2D);
+  glBindTexture(GL_TEXTURE_2D, texturaEnemy);
+  glBegin(GL_TRIANGLE_FAN);
+    glTexCoord2f(0, 0); glVertex3f(0,0 ,0);
+    glTexCoord2f(1, 0); glVertex3f(30,0,0);
+    glTexCoord2f(1, 1); glVertex3f(30,30,0);
+    glTexCoord2f(0, 1); glVertex3f(0,30,0);
+  glEnd();
+  glDisable(GL_TEXTURE_2D);
 }
 
-void drawEnemie(struct Enemie enemie){
+void shotFollows(){
 
-/*  if(enemieColidiu==1){
-    enemieColidiu=0;
-          if(enemieVector[j].y>enemieVector[i].y){
-              enemie.y+=50;
-          }
-          else
-              enemie.y-=50;
-
-        if(enemieVector[j].y<enemieVector[i].x){
-              enemie.x+=50;
-      }
-      else
-              enemie.x-=50;
-  }*/
-
-    glColor3f(0,1,0);
-  	glBegin(GL_POLYGON);
-        	glVertex2f(enemie.x-15,enemie.y+15);
-          glColor3f(1,0,1);
-        	glVertex2f(enemie.x-15,enemie.y-15);
-        	glVertex2f(enemie.x+15,enemie.y-15);
-          glVertex2f(enemie.x+15,enemie.y+15);
-  glEnd();
+    float vetorx = (cliqueMouse.x - tiro.x);
+    float vetory = (cliqueMouse.y - tiro.y);
+    float  norma = sqrt(vetorx*vetorx + vetory*vetory);
+    vetorx /= norma;
+    vetory /= norma;
+    tiro.x+= vetorx*velocidadeTiro;
+    tiro.y+= vetory*velocidadeTiro;
 }
 
 void enemyFollows(int i){
 
-  float vetorx = axisx - enemieVector[i].x;
-  float vetory = axisy - enemieVector[i].y;
-  float vetorx2 = enemieVector[parametroJ].x - enemieVector[i].x;
-  float vetory2 = enemieVector[parametroJ].y - enemieVector[i].y;
+  float vetorx = axisx - enemyVector[i].x;
+  float vetory = axisy - enemyVector[i].y;
   int  norma = sqrt(vetorx*vetorx + vetory*vetory);
-  int  norma2 = sqrt(vetorx2*vetorx2 + vetory2*vetory2);
   vetorx /= norma;
   vetory /= norma;
-  vetorx2 /= norma2;
-  vetory2 /= norma2;
 
-  enemieVector[i].x += (vetorx)*(1.89f)-(vetorx2);
-  enemieVector[i].y += (vetory)*(1.89f)-(vetory2);
+  enemyVector[i].x += (vetorx)*(1.89f);
+  enemyVector[i].y += (vetory)*(1.89f);
 }
 
 void desenhaCena(void){
     glClear(GL_COLOR_BUFFER_BIT);
+    backGround();
     // Começa a usar a cor azul
     corrigeMouse(posicaoMouse.x,posicaoMouse.y,&trueMouseX,&trueMouseY,globalHeight);
-    glColor3f(.5, .5, 1);
 	  movimenta(up,down,left,right,&axisx,&axisy);
     glPushMatrix();
-        // Faz o objeto se orientar pelo mouse
-        glTranslatef(x, y, 0);
-        glRotatef(atan2(trueMouseY-axisy,trueMouseX-axisx)*180/M_PI, 0, 0, 1);
+      // Faz o objeto se orientar pelo mouse
+      glTranslatef(x, y, 0);
+      glRotatef(atan2(trueMouseY-axisy,trueMouseX-axisx)*180/M_PI, 0, 0, 1);
+      // Desenha o personagem principal
+      characterShape(&colidiu);
+    glPopMatrix();
 
-        // Desenha o personagem principal
-        characterShape(&colidiu);
-        glPopMatrix();
-
-        if(enemieCount < level){
-            randomPosition(&enemieX,&enemieY);
-
-            createEnemie();
-            enemieCount++;
-        }
-    for(int i=0;i<enemieCount;i++){
-      drawEnemie(enemieVector[i]);
+    for(int i=0;i<enemyCount;i++){
+      glPushMatrix();
+        glTranslatef(enemyVector[i].x,enemyVector[i].y,0);
+        drawEnemy();
+      glPopMatrix();
     }
+    if(atirou==1){ //se atirou, o tiro está na tela e cancela o atirou
+      createShot();
+      atirou=0;
+      tiroNaTela=1;
+    }
+    if(tiroNaTela==1){ //se o tiro está na tela, atualiza a posicao do tiro.
+        glPushMatrix();
+        glTranslatef(tiro.x,tiro.y,0);
+        drawShot();
+        glPopMatrix();
+      }
 
-    // Diz ao OpenGL para colocar o que desenhamos na tela
+    if(pause==1){
+      drawPauseScreen();
+    }
     glutSwapBuffers();
 }
 
@@ -186,7 +298,11 @@ void desenhaCena(void){
 void inicializa(void){
     // cor para limpar a tela
     glClearColor(0, 0, 0, 0);      // preto
-
+    createEnemy();
+    loadBackground();
+    loadEnemy();
+    loadPause();
+    loadCharacter();
     // imprime instruções
     printf("Instrucoes:\n");
     printf("\t+: gira no sentido horario\n");
@@ -207,32 +323,32 @@ void redimensiona(int w, int h){
 }
 
 void atualiza(int idx) {
+  if(pause==0){
+    GLfloat orientacaoEmRadianos = grausParaRadianos(orientacaoEmGraus);
 
-  GLfloat orientacaoEmRadianos = grausParaRadianos(orientacaoEmGraus);
-  if(clicou==1){
-      printf("\n\nAs coordenadas do clique sao: x=%d, y=%d",cliqueMouse.x,cliqueMouse.y);
-      clicou=0;
-  }
-  for(int i=0;i<enemieCount;i++){ //LOOP PARA CONFIGURAR OS INIMIGOS
-    if(colisao(i)==1){
-      colidiu=1;
+    if(tiroNaTela==1){
+      tiro_AvaliaPosicao();
     }
-    for(parametroJ=0;parametroJ<enemieCount;parametroJ++){
-      if(parametroJ!=i && colisaoEnemie(enemieVector[parametroJ],enemieVector[i])==1){
-        enemieColidiu=1;
-        enemyFollows(i);
+    for(int i=0;i<enemyCount;i++){ //LOOP PARA CONFIGURAR OS INIMIGOS
+      if(colisao(i)==1){          //colisao com o player
+        colidiu=1;
       }
+      enemyFollows(i);
+
     }
-    enemyFollows(i);
+      x = axisx+ cos(orientacaoEmRadianos);
+      y = axisy+ sin(orientacaoEmRadianos);
 
+      orientacaoEmGraus += velocidadeAngular;
+      glutPostRedisplay();
   }
-    x = axisx+ cos(orientacaoEmRadianos);
-    y = axisy+ sin(orientacaoEmRadianos);
+  else{
+    drawPauseScreen();
+    glutSwapBuffers();
+  }
+  glutTimerFunc(33, atualiza, 0);
 
-    orientacaoEmGraus += velocidadeAngular;
 
-    glutPostRedisplay();
-    glutTimerFunc(33, atualiza, 0);
 }
 // Callback de evento de teclado
 void teclado(unsigned char key, int x, int y){
@@ -283,6 +399,13 @@ void teclado(unsigned char key, int x, int y){
         case '_':
             velocidadeAngular -= 0.5;
             break;
+        case 'p':
+            if(pause==0){
+              pause=1;
+            }
+            else{
+              pause=0;
+            }
         default:
             break;
     }
@@ -299,6 +422,7 @@ int main(int argc, char **argv){
     // Abre a janela
     glutCreateWindow("KILL THE WALKERS!");
 
+
     // Registra callbacks para alguns eventos
     glutDisplayFunc(desenhaCena);
     glutReshapeFunc(redimensiona);
@@ -307,6 +431,7 @@ int main(int argc, char **argv){
     glutMouseFunc(clickMouse);
     glutTimerFunc(0, atualiza, 0);
     inicializa();
+    glutTimerFunc(0,enemySpawn,0);
 
     // Entra em loop e nunca sai
     glutMainLoop();
